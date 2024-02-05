db68d91356dbcbc1e95d4b2a62882d27657c4fd6
wscnd <72019998+wscnd@users.noreply.github.com>
2024-02-04 22:45:29 -0300

    send updated errors to failed queue

--- apps/api-orders/src/orders/order.consumer.ts
+++ apps/api-orders/src/orders/order.consumer.ts
@@ -20,28 +20,40 @@ export class OrderConsumer {
     queue: process.env.RABBITMQ_QUEUE_ORDER_UPDATED,
   })
   async consume(message: IRabbitOrderMessage) {
-    try {
-      switch (message.status) {
-        case OrderStatus.PAID:
-          await this.orderService.pay(message.order_id);
-          return;
-        case OrderStatus.FAILED:
-          await this.orderService.fail(message.order_id);
-          return;
-        default:
-          throw new InvalidStatusError('invalid status');
-      }
-    } catch (e) {
-      await this.amqpConnection.publish(
-        process.env.RABBITMQ_EXCHANGE_ORDERS,
-        process.env.RABBITMQ_RK_ORDER_FAILED,
-        {
-          error: e.message,
-          order_id: message.order_id,
-        },
+    if (!message) {
+      await this.sendToError(
+        new InvalidStatusError('invalid message'),
+        message,
       );
+    } else {
+      try {
+        switch (message.status) {
+          case OrderStatus.PAID:
+            await this.orderService.pay(message.order_id);
+            return;
+          case OrderStatus.FAILED:
+            await this.orderService.fail(message.order_id);
+            return;
+          default:
+            throw new InvalidStatusError('invalid status');
+        }
+      } catch (e) {
+        await this.sendToError(e, message);
+      }
     }
+
     return new Nack(false);
+  }
+
+  async sendToError(e: Error, message) {
+    await this.amqpConnection.publish(
+      process.env.RABBITMQ_EXCHANGE_ORDERS,
+      process.env.RABBITMQ_RK_ORDER_FAILED,
+      {
+        error: e.message,
+        order_id: message?.order_id,
+      },
+    );
   }
 }
 
